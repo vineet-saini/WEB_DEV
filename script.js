4bb79a41952315a70801f3c32f8d7ef09af5d509
Vineet <vineet963.be22@chitkara.edu.in>
2025-07-09 10:27:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// var a = 123;
+
+// console.log(a);
+// function abc(){
+//     console.log("This is abc function");
+//     function hgf(){
+//         console.log("This is hgf");
+
+//     }
+//     hgf();
+// }
+
+// def();
+// function def(){
+//     console.log("This is def function");
+// }
+
+// abc();
+
+// var b =300;
+// console.log(b);
+
+
+// var a =200;
+// console.log(a);
+
+// xyz();
+
+// var xyz = ()=>{
+//     console.log("This is xyz function");
+// }
+
+// console.log(q);
+// var q = 300;
+
+
+
+// {
+    //Compound Statement
+    
+//     var a =100;
+//     var b = 200;
+//     var c = 300;
+//     {
+//         console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     {
+//         let d = 400;
+
+//     }
+//     }
+// }
+
+
+// function abc(){
+//     var a = 100;
+//     var b = 200;
+//     var c = 300;
+//     function def(){
+//         var d = 400;
+//         function ghi(){
+//             var e = 500;
+//             console.log(a);
+//             console.log(b);
+//             console.log(c);
+//             console.log(d);
+//             console.log(e);
+//         }
+//         ghi();
+//     }
+//     def();
+// }
+// abc();
+
+// for(var i = 0; i <= 5; i++){
+//     function closure(i){
+//         setTimeout(() => {
+//             console.log(i);
+//         },2000);
+//     }
+// }
+
+
+// function xyz(cb){
+//     console.log("This is xyz function");
+//     cb();
+// }
+
+// function cb(){
+//     console.log("This is cd function");
+// }
+// xyz(cb);
+
+
