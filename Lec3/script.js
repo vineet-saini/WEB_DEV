58dcace6d36108c57ec2b7536799ce1e91313c4f
vineet-saini <123089775+vineet-saini@users.noreply.github.com>
2025-07-09 12:26:34 +0530

    lec3

---
+++ Lec3/script.js
@@ -0,0 +1,50 @@
+// let cart=["shoes","shirt","wallets"];
+
+// function orderDetail(cart){
+//     // Total no of products
+//     // Total Amount of Products nos*1000
+
+//     let totalno = cart.lenth;
+
+//     let totalAmount = totalno * 1000;
+// }
+
+// function orderSummary(){
+//     // total price and total product
+//     // then generate order id
+
+// }
+
+// function paymentGateway(){
+//     // order id , product details and price
+// }
+
+// function successfulOrder(){
+//     // it will print all the details about the order
+// }
+
+// var arr = [2,4,6,8, 100, 58, 65];
+
+// Array.prototype.filterReplica = function(callback){
+//     let output = [];
+//     for(let i = 0; i<this.length; i++){
+//         if(callback(this[i])){
+//             output.push(this[i]);
+//         }
+//     }
+//     return output;
+// }
+
+// let filterArray = arr.filterReplica(function(value){
+//     return value > 50;
+// });
+// console.log(filterArray);
+
+
+
+
+var arr = [1 ,2, 3, 4];
+
+Array.prototype.Accum = function(acc, curr){
+    
+}
